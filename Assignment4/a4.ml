6eb1f1720c23e273fada1c557593cc9001afa792
rajat499 <cs5170415@cse.iitd.ac.in>
2019-03-29 21:46:30 +0530

    Add Comments

--- Assignment4/a4.ml
+++ Assignment4/a4.ml
@@ -7,6 +7,7 @@ exception IllegalList
 (* hastype : ((string * exptype) list) -> exptree -> exptype -> bool *)
 (* let rec hastype g e t = raise Not_implemented *)
 
+(*Given a variable x, it looks for x in table/list g and returns it's type*)
 let rec giveType g x = match g with
                         [] -> raise TypeOfVariableNotFound
                       | hd :: tl -> let (a,b) = hd in
@@ -20,40 +21,8 @@ let rec get_ith list i = if(i<0) then (raise IllegalList) else
                               |1 -> List.hd list
                               |_ -> get_ith (List.tl list) (i-1))
 
-(* let rec getType g e =  let rec tupleType t = (match t with [] -> []
-                                                            | hd:: tl -> (getType g hd):: (tupleType tl)) in
-                      (match e with
-                        Var(x) -> (giveType g x)
-                      | N(i)   -> Tint
-                      | B(b)   -> Tbool
-                      | Abs(t) -> getType g t
-                      | Negative(t) -> getType g t
-                      | Not(t)      -> getType g t
-                      | Add(t1, t2) -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tint else raise BadType
-                      | Sub(t1, t2) -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tint else raise BadType
-                      | Mult(t1, t2)-> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tint else raise BadType
-                      | Div(t1, t2) -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tint else raise BadType
-                      | Rem(t1, t2) -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tint else raise BadType
-                      | Conjunction(t1, t2) -> if (((getType g t1) = Tbool) &&  ((getType g t2) = Tbool)) then Tbool else raise BadType
-                      | Disjunction(t1, t2) -> if (((getType g t1) = Tbool) &&  ((getType g t2) = Tbool)) then Tbool else raise BadType
-                      | Equals(t1, t2)      -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tbool else raise BadType
-                      | GreaterTE(t1, t2)   -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tbool else raise BadType
-                      | LessT(t1, t2)       -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tbool else raise BadType
-                      | GreaterT(t1, t2)    -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tbool else raise BadType
-                      | LessTE(t1, t2)      -> if (((getType g t1) = Tint) &&  ((getType g t2) = Tint)) then Tbool else raise BadType
-                      | InParen(t)          -> (getType g t)
-                      | IfThenElse(t1,t2,t3)-> let ty = (getType g t2) in if (((getType g t1) = Tbool) &&  ((getType g t3) = ty)) then ty else raise BadType
-                      | Tuple(n, x) -> Ttuple(tupleType x)
-                      | Project((i,n), x) -> let Tuple(a,b) = x in 
-                                             let l = (tupleType b) in                                                         
-                                              if(n= (List.length l)) then (get_ith l i) else raise BadType
-                      | FunctionAbstraction(s, x) ->  let ty  = (giveType g s) in Tfunc(ty, getType g x)
-                      | FunctionCall (t1, t2) -> let x = (getType g t2) in
-                                                 let Tfunc(y,z) = (getType g t1) in
-                                                 if(y = x) then z else raise BadType 
-                      | Let(d,x) ->let g_dash = (getYield g d) in (getType (g_dash::g) x)
-                      ) *)
-
+(*Takes a table gamma and a definition d and returns the augment gamma'
+In case of Sequence gamma1[gamma2] is returned as well as in case of parallel*)
 let rec getYield g d = match d with
                      Simple(s,e) -> [(s, (getType g e))]
                     |Parallel(l) -> (match l with 
@@ -66,6 +35,8 @@ let rec getYield g d = match d with
                                     )
                    |Local(d1,d2)-> getYield ((getYield g d1) @ g) d2
 
+(*Takes an exptree and a table gamma and tells the type of exptree.
+getType and getYield are two mutually recursive function.*)
 and getType g e =  let rec tupleType t = (match t with [] -> []
                                           | hd:: tl -> (getType g hd):: (tupleType tl)) in
                                           (match e with
@@ -100,45 +71,7 @@ and getType g e =  let rec tupleType t = (match t with [] -> []
                                           | Let(d,x) ->let g_dash = (getYield g d) in (getType (g_dash @ g) x)
                                           )
                    
-(*                       
-let rec hastype g e t =   try
-                          ( match e with
-                            Var(x) -> if ((giveType g x) = t) then true else false
-                          | N(i)   -> if t = Tint then true else false
-                          | B(b)   -> if t = Tbool then true else false
-                          | Abs(x) -> (hasType g x Tint) && (t = Tint)
-                          | Negative(x) -> (hasType g x Tint) && (t = Tint)
-                          | Not(x) -> (hasType g x Tbool) && (t = Tbool)
-                          | Add(x1, x2) -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tint)
-                          | Sub(x1, x2) -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tint)
-                          | Mult(x1, x2)-> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tint)
-                          | Div(x1, x2) -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tint)
-                          | Rem(x1, x2) -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tint)
-                          | Conjunction(x1, x2) -> (hasType g x1 Tbool) && (hasType g x2 Tbool) && (t = Tbool)
-                          | Disjunction(x1, x2) -> (hasType g x1 Tbool) && (hasType g x2 Tbool) && (t = Tbool)
-                          | Equals(x1, x2)      -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tbool)
-                          | GreaterTE(x1, x2)   -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tbool)
-                          | LessTE(x1, x2)      -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tbool)
-                          | GreaterT(x1, x2)    -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tbool) 
-                          | LessT(x1, x2)       -> (hasType g x1 Tint) && (hasType g x2 Tint) && (t = Tbool)
-                          | InParen(x)          -> (hasType g x t)
-                          | IfThenElse(x1, x2, x3) -> (hasType g x1 Tbool) && (hasType g x2 t) && (hasType g x3 t)
-                          | Tuple(n, x) -> (match x with
-                                            
-
-                                            )
-                          | Project(i,n,x) ->
-                          | Let(d,x) ->
-                          | FunctionAbstraction(s, x) ->  let Tfunc(tau1, tau2)  = t in
-                                                          let type = (giveType g s) in 
-                                                          (type = tau1) && (hasType ((s,type)::g) x tau2)
-                          | FunctionCall(x1, x2) -> ((getType g e) = t)
-                          )
-                          with TypeException -> false
-                          with IllegalList -> false *)
-
-
-
+(*Takes a table gamma and expression e and type t and tells whether type of e is t or not*)
 let rec hastype g e t =   try
                           ( match e with
                               FunctionAbstraction(s,x) -> let Tfunc(t1, t2) = t in (getType ((s,t1)::g) x) = t2
@@ -147,12 +80,13 @@ let rec hastype g e t =   try
                           with BadType -> false
                           | IllegalList -> false
  
-                          
+(*Takes two table and tells whether they are equivalent or not*)                          
 let rec compareExpTypeList l1 l2 = match l1 with
                                   [] -> false
                                 | hd::tl -> let (a,b) = hd in if(tl=[]) then ((giveType l2 a) = b) else (((giveType l2 a) = b) && (compareExpTypeList tl l2))
 
-
+(*Takes a table gamma and definition d and tells when definition d is augmented on gamma then 
+obtained table is equivalent to gamma dash or not*)
 (* yields : ((string * exptype) list) -> definition -> ((string * exptype) list) -> bool *)
 let rec yields g d g_dash = try
                             ( let g_prime = (getYield g d) in  (compareExpTypeList g_prime g_dash))
